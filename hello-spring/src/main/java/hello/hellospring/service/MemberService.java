2b2aef8249bd0ef6a1d0f1d81f74079e7da00344
KyungsooLee <jmtkd9196@gmail.com>
2023-12-07 03:13:48 +0900

    [섹션 3] 회원 관리 예제 - 백엔드 개발 (회원 서비스 테스트)

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,13 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // MemberRepository를 new해서 생성하는게 아니라 외부에서 넣어주도록 변경
+    // 이게 바로 DI(의존성 주입)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -27,7 +33,7 @@ public class MemberService {
                     // ifPresent() : Optional일 때 사용 가능하며, null이 아니라 이미 값이 있으면 안에 있는 코드가 동작한다.
                     // 그냥 꺼내고 싶으면 get()을 사용하면 된다. (옵셔널일 경우에 보통 .get()을 사용해서 바로 꺼내는 것을 권장하지는 않는다.)
                     // 실무에서는 orElseGet()을 많이 사용하는데. 이 함수는 값이 없으면 orElseGet() 안에 있는 메서드를 실행해... 뭐 이런식으로 옵셔널 바인딩을 한다.
-                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
